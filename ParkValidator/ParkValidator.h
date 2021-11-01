a1577c01d67299e61e28122bdf659f223bad3cc5
boardguy1024 <boardguy1024@gmail.com>
2021-11-02 00:13:57 +0900

    Initial Commit

---
+++ ParkValidator/ParkValidator.h
@@ -0,0 +1,18 @@
+//
+//  ParkValidator.h
+//  ParkValidator
+//
+//  Created by park kyung seok on 2021/11/02.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ParkValidator.
+FOUNDATION_EXPORT double ParkValidatorVersionNumber;
+
+//! Project version string for ParkValidator.
+FOUNDATION_EXPORT const unsigned char ParkValidatorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ParkValidator/PublicHeader.h>
+
+
